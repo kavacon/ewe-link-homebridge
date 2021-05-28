0e8f665c34e7c7c39a2c5b9696203343e82c155e
Constantinos Kavadias <kavacon@icloud.com>
2021-05-28 22:15:01 +1000

    make connection class able to deal with delayed authentication
    Signed-off-by: Constantinos Kavadias <kavacon@icloud.com>

--- typescript/src/ewelink-connection.ts
+++ typescript/src/ewelink-connection.ts
@@ -33,6 +33,7 @@ export class EwelinkConnection implements Connection {
     readonly params: ConnectionParams;
     readonly logger: Logging;
     private socket: any;
+    private authenticated: boolean = false;
 
     constructor(props: ConnectionParams, logger: Logging) {
         this.params = props;
@@ -42,38 +43,44 @@ export class EwelinkConnection implements Connection {
 
     activateConnection<T>(onSuccess: (auth: LoginInfo) => void): Promise<any> {
         return this._connection.login()
+            .then(auth => {this.authenticated = (auth != undefined);return auth})
             .then(onSuccess, this.onFailure("activateConnection"));
     }
 
     requestDevice<T>(deviceId: string, onSuccess: (device: Device) => T): Promise<T | null> {
-       return this._connection.getDevice(deviceId)
-            .then(onSuccess, this.onFailure("requestDevice"));
+       return this.connection()
+           .then( c => c.getDevice(deviceId).then(onSuccess))
+           .catch(this.onFailure("requestDevice"));
     }
 
     requestDeviceState<T>(deviceId: string, onSuccess: (state: DeviceState) => T): Promise<T | null> {
-        return this._connection.getDevicePowerState(deviceId)
-            .then(onSuccess, this.onFailure("requestDeviceState"))
+        return this.connection()
+            .then( c => c.getDevicePowerState(deviceId).then(onSuccess))
+            .catch(this.onFailure("requestDeviceState"));
     }
 
     requestDevices<T>(onSuccess: (devices: Device[]) => T): Promise<T | null> {
-        return this._connection.getDevices()
-            .then(onSuccess, this.onFailure("requestDevices"))
+        return this.connection()
+            .then( c => c.getDevices().then(onSuccess))
+            .catch(this.onFailure("requestDevices"));
     }
 
 
     attemptToggleDevice<T>(deviceId: string, onSuccess: (DeviceState) => T): Promise<T | null> {
-        return this._connection.toggleDevice(deviceId)
-            .then(onSuccess, this.onFailure("attemptToggleDevice"))
+        return this.connection()
+            .then( c => c.toggleDevice(deviceId).then(onSuccess))
+            .catch(this.onFailure("attemptToggleDevice"));
     }
 
     openMonitoringSocket(onChange: (deviceId: string, state: string) => void) {
-        this._connection.openWebSocket(data => {
-            onChange(data["deviceid"], data["params"].switch)
-        }).then(socket => {
-                this.logger.info("Web socket for state monitoring successfully opened");
-                this.socket = socket;
-            },
-            this.onFailure("openMonitoringSocket"));
+        return this.connection()
+            .then(c =>
+                c.openWebSocket(data => {onChange(data["deviceid"], data["params"].switch)})
+                    .then(socket => {
+                        this.logger.info("Web socket for state monitoring successfully opened");
+                        this.socket = socket;
+                    }))
+            .catch(this.onFailure("openMonitoringSocket"));
     }
 
     closeMonitoringSocket(){
@@ -83,8 +90,23 @@ export class EwelinkConnection implements Connection {
     private onFailure(method: string){
         return (reason: any) => {
             this.logger.error("The following error was encountered by the eweLink connection " +
-                "while attempting to execute function [%s] %s", method, reason)
+                "while attempting to execute function [%s] %s", method, reason);
             return null;
+        }
+    }
+
+    private connection(attempt: number = 0) : Promise<eWelink> {
+        if (this.authenticated) {
+            return Promise.resolve(this._connection);
+        } else {
+            return new Promise<eWelink>((resolve, reject) => {
+                if (attempt >= 3) {
+                    reject("Connection authentication has not occurred in the last 30 seconds, no more attempts")
+                }
+                setTimeout(() => {
+                    resolve(this.connection(attempt + 1))
+                }, 10000)
+            })
         }
     }
 }
