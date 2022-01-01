2e7607bb93911bf7606f7943f08dc89a23419272
Constantinos Kavadias <kavacon@icloud.com>
2022-01-01 19:57:03 +1100

    add comment giving context on why scheduling is needed
    Signed-off-by: Constantinos Kavadias <kavacon@icloud.com>

--- src/ewelink/ewelink-update-handler.ts
+++ src/ewelink/ewelink-update-handler.ts
@@ -12,6 +12,14 @@ export function handleWebSocketMessage(log: Logging, toleranceWindow: number, da
     }
 }
 
+/**
+ * This function schedules a status update and overrides any previously scheduled updates. Ewelink devices sometimes
+ * send keep alive events that show the switch status as on then immediately off which causes spam events to homebridge
+ * by waiting for a configurable tolerance window we can avoid this spam by overriding the previous update.
+ * @param toleranceWindow the length of time to wait for new updates before executing the current one
+ * @param deviceId the device being updated
+ * @param callback the update callback
+ */
 function scheduleUpdate(toleranceWindow: number, deviceId: string, callback:  () => void) {
     let timeout = deviceUpdates.get(deviceId);
     if (timeout) {
